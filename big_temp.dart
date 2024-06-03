cb2748265685cb5ae2a01651432b9de86f23ecec
Amy <36868219+AmyChengg@users.noreply.github.com>
2024-06-03 00:03:07 -0700

    Update big_temp.dart

--- big_temp.dart
+++ big_temp.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 
 class BigTemp extends StatelessWidget {
   final int tempInFarenheit;
-  const BigTemp(this.tempInFarenheit, {super.key});// TODO(you): write a constructor (hint: look at the other views in this directory)
-
+  const BigTemp(this.tempInFarenheit, {super.key}); // Constructor
   @override
   Widget build(BuildContext context) {
-    // TODO(you): return a widget or widget tree that shows the temperature in big text
+    // Retursn a widget or widget tree that shows the temperature in big text
     // We want to display the current temperature in 36 point, bold text. 
 
-    // Write a constructor for BigTemp and implement its build method.
+    // Constructor for BigTemp and implement its build method.
     return Center(
       child: Text(
         '${tempInFarenheit.toString()}°F',
